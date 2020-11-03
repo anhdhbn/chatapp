19dc470102f7f78f67ff5f7373a9688d6d2645f5
AnhDH <anhdhbn@gmail.com>
2020-11-03 09:38:17 -0800

    test

--- src/test/java/ConfigReaderTest.java
+++ src/test/java/ConfigReaderTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ConfigReaderTest {
 
     @org.junit.jupiter.api.Test
-    void getPropValues() throws IOException {
+    void testGetPropValues() throws IOException {
         ConfigReader cr = new ConfigReader();
         cr.getPropValues();
         Assertions.assertEquals(1699, cr.port);
