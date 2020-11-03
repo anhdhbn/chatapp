708b811305e379173d72beafb4c93e23da654693
AnhDH <anhdhbn@gmail.com>
2020-11-03 09:00:02 -0800

    init

---
+++ src/main/java/ConfigReader.java
@@ -0,0 +1,31 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Properties;
+
+public class ConfigReader {
+    public int port = -1;
+    public void getPropValues() throws IOException {
+        InputStream inputStream = null;
+        try {
+            Properties prop = new Properties();
+            String propFileName = "config.properties";
+
+            inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+            }
+
+            int port = Integer.parseInt(prop.getProperty("port"));
+            this.port = port;
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        } finally {
+            inputStream.close();
+        }
+    }
+}
---
+++ src/test/java/ConfigReaderTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConfigReaderTest {
+
+    @org.junit.jupiter.api.Test
+    void getPropValues() throws IOException {
+        ConfigReader cr = new ConfigReader();
+        cr.getPropValues();
+        Assertions.assertEquals(1699, cr.port);
+    }
+}
