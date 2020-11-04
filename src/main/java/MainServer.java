2d0888e60b58acc328bb70bdbea33d39052e677b
AnhDH <anhdhbn@gmail.com>
2020-11-03 23:04:23 -0800

    test ci/cd

--- src/main/java/MainServer.java
+++ src/main/java/MainServer.java
@@ -2,11 +2,12 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class MainServer {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         ConfigReader cr = new ConfigReader();
         cr.getPropValues();
         System.out.println(cr.port);
-        Scanner myObj = new Scanner(System.in);
-        myObj.nextLine();
+        while (true){
+            Thread.sleep(1000);
+        }
     }
 }
