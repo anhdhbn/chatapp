2872a4e8a74b377d284578ecec321bf17bc00f9f
AnhDH <anhdhbn@gmail.com>
2020-11-03 22:58:09 -0800

    test ci/cd

--- src/main/java/MainServer.java
+++ src/main/java/MainServer.java
@@ -1,9 +1,12 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class MainServer {
     public static void main(String[] args) throws IOException {
         ConfigReader cr = new ConfigReader();
         cr.getPropValues();
         System.out.println(cr.port);
+        Scanner myObj = new Scanner(System.in);
+        myObj.nextLine();
     }
 }
