585c9f101b5a750e34e89d3b3bac696c0e800ec5
tungtobi <tungtobi@gmail.com>
2020-11-09 16:52:07 +0700

    save file

--- src/main/java/npclient/gui/controller/FileMessageController.java
+++ src/main/java/npclient/gui/controller/FileMessageController.java
@@ -3,8 +3,15 @@ package npclient.gui.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import npclient.gui.manager.StageManager;
+import npclient.gui.util.UIUtils;
 import nputils.FileInfo;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,16 +22,22 @@ public class FileMessageController implements Initializable {
     @FXML
     private Text tFileSize;
 
+    private FileInfo fileInfo;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 
     public void setFileInfo(FileInfo fileInfo) {
+        this.fileInfo = fileInfo;
         this.tFileName.setText(fileInfo.getName());
+        String fileSize = generateFileSize(fileInfo.getSize());
+        this.tFileSize.setText(fileSize);
+    }
 
-        String unit = "byte";
-        long size = fileInfo.getSize();
+    private static String generateFileSize(long size) {
+        String unit = "bytes";
         if (size > 1024) {
             size /= 1024;
             unit = "KB";
@@ -33,6 +46,31 @@ public class FileMessageController implements Initializable {
             size /= 1024;
             unit = "MB";
         }
-        this.tFileSize.setText(String.format("%d %s", size, unit));
+
+        return String.format("%d %s", size, unit);
+    }
+
+    @FXML
+    public void onDownload() {
+        if (fileInfo != null) {
+            FileChooser chooser = new FileChooser();
+            chooser.setTitle("Save file");
+            chooser.setInitialFileName(fileInfo.getName());
+            Stage primaryStage = StageManager.getInstance().getPrimaryStage();
+            File file = chooser.showSaveDialog(primaryStage);
+            if (file != null) {
+                save(file);
+            }
+        } else {
+            UIUtils.showErrorAlert("Can't find file's specification");
+        }
+    }
+
+    private void save(File file) {
+        try (FileOutputStream stream = new FileOutputStream(file)) {
+            stream.write(fileInfo.getData());
+        } catch (IOException e) {
+            UIUtils.showErrorAlert("Can't save file " + file.getName() + ": " + e.getMessage());
+        }
     }
 }
